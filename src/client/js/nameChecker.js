1f9dd972cd09bb47813184152cd3ad8a0797ce15
peffjetzke <84154579+peffjetzke@users.noreply.github.com>
2021-07-16 15:19:51 -0700

    initial commit pt 2

---
+++ src/client/js/nameChecker.js
@@ -0,0 +1,8 @@
+//check for a valid URL here
+function isValidURL(url){ 
+    console.log("Validating URL: ", url);
+
+    return url;     //stubbed in function, should just pass through, need to figure out validating URL 
+}
+
+export { isValidURL }
