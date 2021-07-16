5184c1ebba1b3ddeb7ce57cd28b1d749ad14270e
peffjetzke <84154579+peffjetzke@users.noreply.github.com>
2021-07-16 15:21:10 -0700

    Delete src/client directory

--- src/client/js/nameChecker.js
+++
@@ -1,8 +0,0 @@
-//check for a valid URL here
-function isValidURL(url){ 
-    console.log("Validating URL: ", url);
-
-    return url;     //stubbed in function, should just pass through, need to figure out validating URL 
-}
-
-export { isValidURL }
